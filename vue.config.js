a763f8675544811cce4e575883540c40b90307dd
wing0607 <731771480@qq.com>
2020-06-03 15:13:01 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        //target: 'http://192.168.0.68:8992',    // 目标 API 地址
+        //target: 'http://192.168.0.68:8992',   
         target: 'http://192.168.0.113:8992',
         changeOrigin: true,
         pathRewrite: {
